946f488a6e35d8667330eb7eac103b5ddef5c0c4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-11 22:22:16 -0300

    Box

--- app/src/main/java/guilherme26bc/com/github/MainActivity.kt
+++ app/src/main/java/guilherme26bc/com/github/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import guilherme26bc.com.github.ui.theme.FundamentosJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,6 +48,26 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
+    fun NotificationBage(modifier: Modifier = Modifier) {
+        Box(modifier = modifier.padding(16.dp), contentAlignment = Alignment.Center) {
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+                    .clip(CircleShape)
+                    .background(color = Color.Green)
+            )
+            Box(
+                modifier = Modifier
+                    .size(8.dp)
+                    .clip(CircleShape)
+                    .background(color = Color.Red)
+                    .align(Alignment.TopEnd)
+            )
+        }
+
+    }
+
+    @Composable
     fun TextoCustomizado(texto: String, modifier: Modifier = Modifier) {
         Text(
             text = texto,
@@ -55,6 +81,14 @@ class MainActivity : ComponentActivity() {
     fun PreviewFuncaoInicial() {
         FundamentosJetpackComposeTheme {
             Inicial()
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewNotificationBage() {
+        FundamentosJetpackComposeTheme {
+            NotificationBage()
         }
     }
 }
