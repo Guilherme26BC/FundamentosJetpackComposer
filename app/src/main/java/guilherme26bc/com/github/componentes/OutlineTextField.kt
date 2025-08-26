7db08348a8c2d2709e481ecd0140fa3589f9c9fa
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-25 22:09:03 -0300

    outlineTextField e atributo isError 2

--- app/src/main/java/guilherme26bc/com/github/componentes/OutlineTextField.kt
+++ app/src/main/java/guilherme26bc/com/github/componentes/OutlineTextField.kt
@@ -1,5 +1,6 @@
 package guilherme26bc.com.github.componentes
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,21 +10,62 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 
 @Preview
 @Composable
 fun SimpleOutlineTextField(modifier: Modifier= Modifier) {
-    var text by remember { mutableStateOf("") }
-    var isError by remember { mutableStateOf(false) }
+    var text1 by remember { mutableStateOf("") }
+    var text2 by remember { mutableStateOf("") }
+    var isError1 by remember { mutableStateOf(false) }
+    val focusRequester2 = remember { FocusRequester() }
+    var isSecondFocused by remember { mutableStateOf(false) }
 
-    OutlinedTextField(
-        modifier = modifier,
-        value = text,
-        onValueChange = { novoValorDigitado: String ->
-            text = novoValorDigitado
-            isError = novoValorDigitado.contains("0")
-        },
-        label = { Text(text = "Nome") },
-        isError = isError,
-    )
+//    OutlinedTextField(
+//        modifier = modifier,
+//        value = text1,
+//        onValueChange = { novoValorDigitado: String ->
+//            text1 = novoValorDigitado
+//            isError1 = novoValorDigitado.contains("0")
+//        },
+//        label = { Text(text = "Nome") },
+//        isError = isError1,
+//    )
+
+    Column(modifier = modifier) {
+            OutlinedTextField(
+                modifier = Modifier,
+                value = text1,
+                onValueChange = {novoValorDigitado: String ->
+                    text1 = novoValorDigitado
+                    if(isSecondFocused){
+                        isError1 = novoValorDigitado.isEmpty()
+                    } else {
+                        isError1 = false
+                    }
+                },
+                label = { Text(text = "Nome") },
+                isError = isError1,
+                singleLine = true,
+            )
+
+        OutlinedTextField(
+            modifier = Modifier.focusRequester(focusRequester2)
+                .onFocusChanged { focusState ->
+                    isSecondFocused = focusState.isFocused
+                    if (focusState.isFocused) {
+                        isError1 = text1.isEmpty()
+                    }
+                },
+            value = text2,
+            onValueChange = { novoValorDigitado: String ->
+                text2 = novoValorDigitado
+            },
+            label = { Text(text = "Sobrenome") },
+            singleLine = true,
+        )
+
+    }
 }
