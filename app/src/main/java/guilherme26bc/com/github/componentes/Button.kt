15c90c7329f9403ab527a3ea56a4baad3f9dc569
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-08 22:04:03 -0300

    Lidando com cliques em botoes

--- app/src/main/java/guilherme26bc/com/github/componentes/Button.kt
+++ app/src/main/java/guilherme26bc/com/github/componentes/Button.kt
@@ -8,6 +8,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -15,11 +19,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview
+//@Preview
 @Composable
-fun SimpleButton() {
+fun SimpleButton(count: Int, onClick: () -> Unit) {
     Button(
-        onClick = { /*TODO*/ }, modifier = Modifier
+        onClick = { onClick }, modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
     ) {
@@ -29,6 +33,8 @@ fun SimpleButton() {
 @Preview
 @Composable
 fun InitialScreen(modifier: Modifier = Modifier) {
+    var count by remember { mutableStateOf(0) }
+
     Column(modifier = modifier.fillMaxSize()) {
         // Content
         Column(
@@ -37,12 +43,12 @@ fun InitialScreen(modifier: Modifier = Modifier) {
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "Quantidade de cliques",
+                text = "Quantidade de cliques $count",
                 fontSize = 20.sp,
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center
             )
         }
-        SimpleButton()
+        SimpleButton(count = count, onClick = {count++})
     }
 }
